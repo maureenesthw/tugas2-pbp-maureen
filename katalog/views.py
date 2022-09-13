07e736438ce0694f6bfb461a6917d71212dc834e
maureenesthw <maureen.esther.wijaya@gmail.com>
2022-09-13 16:37:41 +0700

    render, route, load data

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'item_list' : data_katalog,
+        'name' : 'Maureen Esther Wijaya',
+        'npm' : '2106705335'
+    }
+    return render(request, "katalog.html", context)
