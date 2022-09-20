a3a70da481631a149f29c380ab39c0858c201597
maureenesthw <maureen.esther.wijaya@gmail.com>
2022-09-21 01:48:27 +0700

    implement has_watched_a_lot

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -5,9 +5,25 @@ from django.core import serializers
 
 # Create your views here.
 def show_mywatchlist(request):
+    data_watchlist = WatchList.objects.all()
+    total_watched = 0
+    total_not_watched = 0
+    has_watched_a_lot = False
+
+    for data in data_watchlist:
+        if data.watched:
+            total_watched+=1
+        else:
+            total_not_watched+=1
+
+    if total_watched >= total_not_watched:
+        has_watched_a_lot = True
+    
     context = {
+        'list_watchlist': data_watchlist,
         'name': 'Maureen Esther Wijaya',
-        'npm': '2106705335'
+        'npm': '2106705335',
+        'has_watched_a_lot': has_watched_a_lot
     }
     return render(request, "mywatchlist.html", context)
 
@@ -24,7 +40,5 @@ def show_html(request):
     data_watchlist = WatchList.objects.all()
     context = {
         'list_watchlist': data_watchlist,
-        # 'name': 'Maureen Esther Wijaya',
-        # 'npm': '2106705335'
     }
     return render(request, "mywatchlist_data.html", context)
